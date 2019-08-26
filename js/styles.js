cce29e3780f268165233fc4ab1e1afc4f57b07f4
brayooh <brianndichu.bn@gmail.com>
2019-08-26 11:19:35 +0300

    add boarder to portfolio images

--- js/styles.js
+++ js/styles.js
@@ -6,3 +6,10 @@ $('.main').click(function () {
     $(this).children('.hide').slideToggle(1200);
     $(this).children('.show').toggle(1500);
    });
+
+   $(".portfolio img").hover(function(){
+       $(this).animate({opacity: .5})
+   }, function(){
+           $(this).stop().animate({opacity: 1})
+       })
+    
