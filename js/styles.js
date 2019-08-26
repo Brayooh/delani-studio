1b34e520c1fe3e36dc9bd9d9939e005f665da1f6
brayooh <brianndichu.bn@gmail.com>
2019-08-26 14:24:04 +0300

    add hover effect on images

--- js/styles.js
+++ js/styles.js
@@ -1,6 +1,7 @@
 $('.main').click(function () {
     $(this).children('.hide').slideToggle(1200);
     $(this).children('.show').toggle(1500);
+    $(this).children('.hover').toggle();
    });
    
 
