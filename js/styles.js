e8a281eb8b7b4ca31884e9211b54fedc3b61c45a
brayooh <brianndichu.bn@gmail.com>
2019-08-26 12:30:32 +0300

    correction on hover

--- js/styles.js
+++ js/styles.js
@@ -2,10 +2,7 @@ $('.main').click(function () {
     $(this).children('.hide').slideToggle(1200);
     $(this).children('.show').toggle(1500);
    });
-   $('.main').click(function () {
-    $(this).children('.hide').slideToggle(1200);
-    $(this).children('.show').toggle(1500);
-   });
+   
 
    $(".portfolio img").hover(function(){
        $(this).animate({opacity: .5})
